737bbb0b8586b39a3ab7048144af32ba978b22d2
patomation <patomation@gmail.com>
2020-07-24 16:51:14 +0700

    feat: last state of game

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,14 @@ import { Sprite } from './classes/Sprite'
 import ufo from '../assets/sprites/ufo.png'
 import { hotkey } from '@patomation/hotkey'
 import { up, down, left, right } from './lib/input'
+
+// import { init } from 'snabbdom/src/init'
+// import { classModule } from 'snabbdom/src/modules/class'
+// import { propsModule } from 'snabbdom/src/modules/props'
+// import { styleModule } from 'snabbdom/src/modules/style'
+// import { eventListenersModule } from 'snabbdom/src/modules/eventlisteners'
+// import { h } from 'snabbdom/src/h' // helper function for creating vnodes
+
 // if (module && module.hot) {
 //   module.hot.accept()
 // }
@@ -14,6 +22,21 @@ window.addEventListener('DOMContentLoaded', () => {
   initCanvas()
   resizeCanvas(window.innerWidth, window.innerHeight)
   // drawSquare()
+
+  // // const div = document.createElement('div')
+  // const container = document.getElementById('container')
+  // const patch = init([classModule, styleModule])
+  // const someFn = () => {
+  //   console.log('yeet')
+  // }
+  // const vnode = h('div#container.two.classes', { on: { click: someFn } }, [
+  //   h('span', { style: { fontWeight: 'bold' } }, 'This is bold'),
+  //   ' and this is just normal text',
+  //   h('a', { props: { href: '/foo' } }, 'I\'ll take you places!')
+  // ])
+  // // Patch into empty DOM element – this modifies the DOM as a side effect
+  // patch(container, vnode)
+
   ufoSprite = addImage(ufo, 200, 100)
   // follow mouse example
   // onMouseMove((x, y) => {
@@ -22,9 +45,7 @@ window.addEventListener('DOMContentLoaded', () => {
   //   ufoSprite.x = x
   //   ufoSprite.y = y
   // })
-
   const moveAmount = 25
-
   up(() => {
     ufoSprite.y -= moveAmount
   })
